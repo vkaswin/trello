e42b321b93b9d48ab542da7438e0159f211641c7
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-01-16 15:09:00 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:vkaswin/vue-trello-app.git master:gh-pages
+git push -f git@github.com:vkaswin/trello.git master:gh-pages
 
 cd -
